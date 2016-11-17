c6cf93471ef5170d755aee4091795d50db326aa2
Lance <nevertiree@126.com>
2016-11-17 22:45:47 +0800

    Spark Python pairRDD mapValues flatMap

--- Operating_System/Distribution_System/Spark/Python/countWord.py
+++ Operating_System/Distribution_System/Spark/Python/countWord.py
@@ -21,6 +21,9 @@ for line in googleLines.collect():
 	print ""
 
 nums = sc.parallelize([1,2,3,4])
+for num in nums.collect():
+	print "%i" %(num)
+
 squared = nums.map(lambda x: x*x).collect()
 
 for num in squared:
@@ -29,5 +32,7 @@ for num in squared:
 words = sc.parallelize(["hello world","hi"])
 char = words.flatMap(lambda word : word.split(" "))
 print char.first()
+
+
 
 sc.stop()
